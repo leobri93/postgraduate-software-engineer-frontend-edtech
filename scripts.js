2e2ca3ffebdcb2373861817d5558c7f271892a6a
Leonardo Brito <santos.leonardobrito@gmail.com>
2025-07-04 18:59:49 -0300

    First frontend version finished

--- scripts.js
+++ scripts.js
@@ -16,7 +16,6 @@ const postItem = (nomeAluno, emailAluno, dataNascimento) => {
     })
       .then((response) => response.json()) 
         .then((data) => { 
-            console.log('Dados do aluno enviados com sucesso:', data);
             insertList(data.id_aluno, data.nome, data.email, data.data_nascimento, data.data_cadastro);
         })
         .catch((error) => {
@@ -31,25 +30,49 @@ const postItem = (nomeAluno, emailAluno, dataNascimento) => {
 */
 const insertButtonDelete = (parent) => {
   let button = document.createElement("button");
-  let txt = document.createTextNode("Deletar");
+  let trashIcon = document.createElement("i");
+  trashIcon.className = "bi bi-trash";
   button.className = "btn btn-danger btn-sm";
   button.id = "deleteStudent";
   button.type = "button"; 
-  button.appendChild(txt);
+  button.setAttribute("data-bs-toggle", "tooltip");
+  button.setAttribute("data-bs-placement", "top");
+  button.setAttribute("data-bs-trigger", "hover");
+  button.setAttribute("title", "Deletar Aluno");
+  button.appendChild(trashIcon);
   parent.appendChild(button);
 }
 
-const insertButtonAtividade = (parent) => {
+const insertButtonStartActivity = (parent) => {
   let button = document.createElement("button");
-  let txt = document.createTextNode("Iniciar Atividade");
+  let playIcon = document.createElement("i");
+  playIcon.className = "bi bi-play-fill";
   button.className = "btn btn-success btn-sm";
   button.id = "startActivity";
   button.type = "button";
-  button.appendChild(txt);
+  button.setAttribute("data-bs-toggle", "tooltip");
+  button.setAttribute("data-bs-placement", "top");
+  button.setAttribute("data-bs-trigger", "hover");
+  button.setAttribute("title", "Iniciar Atividade");
+  button.appendChild(playIcon);
   parent.appendChild(button);
 };
 
-//criar mais um botão para visualizar atividade de um aluno
+const insertButtonListActivity = (parent) => {
+  let button = document.createElement("button");
+  let infoIcon = document.createElement("i");
+  infoIcon.className = "bi bi-info-circle";
+  button.className = "btn btn-secondary btn-sm";
+  button.id = "listActivity";
+  button.type = "button";
+  button.setAttribute("data-bs-toggle", "modal tooltip");
+  button.setAttribute("data-bs-target", "#atividadesModal");
+  button.setAttribute("data-bs-placement", "top");
+  button.setAttribute("data-bs-trigger", "hover");
+  button.setAttribute("title", "Listar Atividades");
+  button.appendChild(infoIcon);
+  parent.appendChild(button);
+};
 
 /*
   --------------------------------------------------------------------------------------
@@ -92,6 +115,22 @@ const startActivity = () => {
   }
 }
 
+/*
+  --------------------------------------------------------------------------------------
+  Função para listar uma atividade de um aluno de acordo com o click no botão listar atividade
+  --------------------------------------------------------------------------------------
+*/
+const listActivity = () => {
+  let list = document.getElementsByClassName("btn btn-secondary btn-sm");
+  let i;
+  for (i = 0; i < list.length; i++) {
+    list[i].onclick = function () {
+      let div = this.parentElement.parentElement;
+      const idItem = div.getElementsByTagName('td')[0].innerHTML;
+      liststudentActivity(idItem)
+    }
+  }
+}
 
 function formatarDataBR(dataISO) {
     if (!dataISO) return '';
@@ -105,8 +144,12 @@ function formatarDataBR(dataISO) {
 }
 
 
-// Função para inserir um novo aluno na tabela
-function insertList(id_aluno, nome, email, data_nascimento, data_cadastro) {
+/*
+  --------------------------------------------------------------------------------------
+  Função para inserir um novo aluno na tabela
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (id_aluno, nome, email, data_nascimento, data_cadastro) => {
     const tabela = document.getElementById('studentsTable').getElementsByTagName('tbody')[0];
     const novaLinha = tabela.insertRow();
 
@@ -119,43 +162,47 @@ function insertList(id_aluno, nome, email, data_nascimento, data_cadastro) {
 
     // Célula de ações
     const cellAcoes = novaLinha.insertCell(-1);
-    insertButtonAtividade(cellAcoes);
+    insertButtonStartActivity(cellAcoes);
     insertButtonDelete(cellAcoes);
+    insertButtonListActivity(cellAcoes);
     
     removeElement()
     startActivity()
+    listActivity()
 }
 
-// Função para lidar com o envio do formulário de cadastro de aluno
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
-    if (form) {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
-
-            // Obtém os valores dos campos
-            const nome = form.querySelector('input[id="nomeAluno"]').value.trim();
-            const email = form.querySelector('input[id="emailAluno"]').value.trim();
-            const dataNascimento = form.querySelector('input[id="dataNascimento"]').value.trim();
-
-            // Exemplo de uso: exibe no console (substitua por sua lógica de requisição)
-            console.log('Nome:', nome);
-            console.log('Email:', email);
-            console.log('Data de Nascimento:', dataNascimento);
-            postItem(nome, email, dataNascimento);
-
-
-            form.querySelector('input[id="nomeAluno"]').value = '';
-            form.querySelector('input[id="emailAluno"]').value = '';
-            form.querySelector('input[id="dataNascimento"]').value = '';
-
-            // Fechar o modal após o envio
-            const modalElement = document.getElementById('exampleModal');
-            const modalInstance = bootstrap.Modal.getInstance(modalElement) || new bootstrap.Modal(modalElement);
-            modalInstance.hide();
+/*
+  --------------------------------------------------------------------------------------
+  Função para carregar a lista de alunos do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getAlunos = () => {
+  fetch('http://127.0.0.1:8080/alunos', {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then((data) => {
+      const tabela = document.getElementById('studentsTable').getElementsByTagName('tbody')[0];
+      tabela.innerHTML = '';
+      if(data.alunos.length === 0) {
+        const novaLinha = tabela.insertRow();
+        const cell = novaLinha.insertCell(0);
+        cell.colSpan = 6; // Colspan para abranger todas as colunas
+        cell.innerText = 'Nenhum aluno cadastrado.';
+      }
+      else
+      {
+        data.alunos.forEach(aluno => {
+        insertList(aluno.id_aluno, aluno.nome, aluno.email, aluno.data_nascimento, aluno.data_cadastro);
         });
-    }
-});
+      }
+    
+    })
+    .catch(error => {
+      console.error('Erro ao obter a lista de alunos:', error);
+      alert('Erro ao carregar a lista de alunos.');
+    });
+}
 
 /*
   --------------------------------------------------------------------------------------
@@ -185,7 +232,6 @@ const startStudentActivity = (id_aluno) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log('Atividade iniciada com sucesso:', data);
       alert('Atividade iniciada com sucesso!');
     })
     .catch(error => {
@@ -194,4 +240,72 @@ const startStudentActivity = (id_aluno) => {
     });
 }
 
-//recuperar atividade de um aluno
+/*
+  --------------------------------------------------------------------------------------
+  Função para listar a atividade de um aluno via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const liststudentActivity = (id_aluno) => {
+  const modalElement = document.getElementById('atividadesModal');
+  const modalBody = modalElement.querySelector('.modal-body');
+  modalBody.innerHTML = ''; // Limpa o conteúdo anterior
+
+  fetch(`http://127.0.0.1:8080/atividades?id_aluno=${id_aluno}`, {
+    method: 'GET'
+  })
+    .then(response => {
+      if(response.status === 404){
+        modalBody.textContent = 'Nenhuma atividade encontrada para este aluno.';
+        const modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement);
+        modalInstance.show();
+      } 
+      return response.json();
+  })
+    .then((data) => {
+      data.atividades.forEach(activity => {
+        const activityItem = document.createElement('div');
+        activityItem.innerHTML = `<b>Atividade ID:</b> ${activity.id_atividade} <br> <b>Status:</b> ${activity.status} <br> <b>Data Inicio:</b> ${formatarDataBR(activity.data_inicio)} <br> <b>Hora Inicio:</b> ${activity.hora_inicio} <br> <b>Duração:</b> ${activity.duracao} <br>`;
+        modalBody.appendChild(activityItem);
+      });
+      const modalInstance = bootstrap.Modal.getOrCreateInstance(modalElement);
+      modalInstance.show();
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+}
+
+// Chama a função de recuperar alunos ao carregar a página
+document.addEventListener('DOMContentLoaded', getAlunos);
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para lidar com o evento de envio do formulário
+  --------------------------------------------------------------------------------------
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('form');
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            // Obtém os valores dos campos
+            const nome = form.querySelector('input[id="nomeAluno"]').value.trim();
+            const email = form.querySelector('input[id="emailAluno"]').value.trim();
+            const dataNascimento = form.querySelector('input[id="dataNascimento"]').value.trim();
+
+            // Exemplo de uso: exibe no console (substitua por sua lógica de requisição)
+            postItem(nome, email, dataNascimento);
+
+
+            form.querySelector('input[id="nomeAluno"]').value = '';
+            form.querySelector('input[id="emailAluno"]').value = '';
+            form.querySelector('input[id="dataNascimento"]').value = '';
+
+            // Fechar o modal após o envio
+            const modalElement = document.getElementById('exampleModal');
+            const modalInstance = bootstrap.Modal.getInstance(modalElement) || new bootstrap.Modal(modalElement);
+            modalInstance.hide();
+        });
+    }
+});
